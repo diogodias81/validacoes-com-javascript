1c08393298c77747b34660976a53f28623d870c4
Diogo Dias Beckman <diogodias2548@gmail.com>
2026-07-14 13:30:27 -0300

    feat: novo funcionalidade de funcao

--- teste 05/script.js
+++ teste 05/script.js
@@ -1,21 +1,26 @@
+const texto1 = document.querySelector('#texto1');
+const texto2 = document.querySelector('#texto2');
+
+
 function moverTexto1ParaTexto2(){
-    const textoUser = document.querySelector('#texto1');
-    
-    
-    if(!textoUser.value){
-        alert("Insira um texto!")
-        return ;
-    }   
-    
-    
-    document.querySelector('#texto2').value = textoUser.value;
-    textoUser.value = "";
+    validarCampo(texto1);
+
+    texto2.value = texto1.value;
+    texto1.value = "";
 }
 
 
 function moverTexto2ParaTexto1(){
-    const textoMovido = document.querySelector('#texto2');
-    document.querySelector('#texto1').value = textoMovido.value
-    textoMovido.value = "";
-    
+    validarCampo(texto2)
+
+    texto1.value = texto2.value
+    texto2.value = "";    
+}
+
+function validarCampo(campo) {
+
+    if(!campo.value){
+        alert(`O campo ${campo.id} é obrigatório.`)
+        return;
+    }
 }
